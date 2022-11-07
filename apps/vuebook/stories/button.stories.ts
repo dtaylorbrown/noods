aee27acc15f92cdd423ee3228809a89ee91ed2ef
dtaylorbrown <daniella.taylor92@gmail.com>
2022-11-07 21:37:44 +0000

    :sparkles: storybook for vue with vite

--- apps/vuebook/stories/button.stories.ts
+++ apps/vuebook/stories/button.stories.ts
@@ -1,13 +1,10 @@
 import { Button } from '@noods/vue';
 
-// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
   title: 'Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
-    onClick: {},
     size: {
       control: { type: 'select' },
       options: ['small', 'medium', 'large'],
@@ -15,22 +12,18 @@ export default {
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
   components: { Button },
-  // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
-    console.log(Button);
+    console.log(args);
     return { args };
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<button v-bind="args" />',
+  template: '<Button v-bind="args" />',
 });
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Vue',
+  onClick: () => { console.log('Boop') }
 };
--- packages/noods-vue/components/Button.vue
+++ packages/noods-vue/components/Button.vue
@@ -1,11 +1,11 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <button type="button" @click="onClick">{{ label }}</button>
 </template>
 
-<script>
-  import { reactive, computed } from 'vue';
+<script lang="ts">
+  import { reactive } from 'vue';
   export default {
-    name: 'button',
+    name: 'Button',
     props: {
       label: {
         type: String,
@@ -17,7 +17,7 @@
       },
       size: {
         type: String,
-        validator: function (value) {
+        validator: function (value: string) {
           return ['small', 'medium', 'large'].indexOf(value) !== -1;
         },
       },
@@ -29,15 +29,6 @@
     setup(props, { emit }) {
       props = reactive(props);
       return {
-        classes: computed(() => ({
-          'storybook-button': true,
-          'storybook-button--primary': props.primary,
-          'storybook-button--secondary': !props.primary,
-          [`storybook-button--${props.size || 'medium'}`]: true,
-        })),
-        style: computed(() => ({
-          backgroundColor: props.backgroundColor,
-        })),
         onClick() {
           emit('click');
         }
