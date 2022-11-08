1a8eda144c65758e54c5da2386584cc98f9074ec
dtaylorbrown <daniella.taylor92@gmail.com>
2022-11-08 11:25:21 +0000

    :sparkles: add changesets

--- .changeset/early-colts-explain.md
+++
@@ -1,6 +0,0 @@
----
-"@noods/react": minor
-"@noods/vue": minor
----
-
-inception
--- packages/noods-react/components/Button.tsx
+++ packages/noods-react/components/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
-export const Button = ({ children }) => {
+export const Button = ({ children }: any) => {
   return <button onClick={() => console.log("Boop")}>{children}</button>;
 };
